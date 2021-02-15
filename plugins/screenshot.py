d2125ed4ef8c2587bacbb977ea75218ff8e6b117
ninjafun20 <69907104+Boy656@users.noreply.github.com>
2021-02-15 21:02:27 +0300

    Update screenshot.py

--- plugins/screenshot.py
+++ plugins/screenshot.py
@@ -5,7 +5,7 @@ from pyrogram.raw import functions
 from pyrogram.types import Message
 
 from userge import userge
-from userge.custom.help import add_command_help
+from userge.misc.help import add_command_help
 
 
 @userge.on_message(
